f6c9cd76eefdef3d96d7d977c5d7b1b6aef1869f
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-19 17:01:24 +0500

    updated heat pumps page

---
+++ heat-pumps.php
@@ -0,0 +1,104 @@
+<?php include_once('header.php') ?>
+
+<title>Naugle Heating and Cooling | Heat Pumps</title>
+
+<meta name="description"
+    content=" When it comes to getting the best high-efficiency heat pump or heat pump repair in Lenawee County, the only name you need to know is Naugle Heating and Cooling.">
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/dual-fuel-lennox-furnace.png"
+                                    alt="duel fuel banner" class="img-responsive">
+
+                                <br><br>
+
+
+                                <p><strong>Naugle Heating and Cooling: Professionally Installed High-Efficiency
+                                        Dual-Fuel Heat
+                                        Pumps for Year-Round Comfort in Michigan Homes</strong></p>
+
+
+
+                                <p>Heat pumps are gaining popularity among Michigan homeowners for their
+                                    dual-functionality
+                                    as heating and cooling systems. When paired with an energy-efficient furnace, they
+                                    create a versatile dual-fuel system, providing comfort year-round. In warmer months,
+                                    the
+                                    heat pump acts as an efficient air conditioner, while in colder months, it extracts
+                                    heat
+                                    from the outdoor air to keep homes warm.</p>
+
+
+
+                                <p>For optimal performance, the heat pump works best above 35 degrees Fahrenheit, with
+                                    the
+                                    furnace taking over as the primary heating source as temperatures drop further.
+                                    Combining a Lennox or Carrier heat pump with a high-efficiency furnace creates an
+                                    energy-saving dual-fuel system, reducing reliance on a single energy source and
+                                    ensuring
+                                    comfort even on the coldest days.</p>
+
+
+
+                                <p>Factors such as HVAC system design, size, maintenance, climate, insulation, and
+                                    occupant
+                                    behavior influence system efficiency. Naugle Heating and Cooling offers expert
+                                    analysis
+                                    and load calculations to help homeowners select the ideal HVAC system, promising
+                                    heightened energy efficiency and comfort.</p>
+
+
+
+                                <h2>Maximize Your Savings with Tax Credits and Incentives</h2>
+                                <p>Did you know? You could be eligible for substantial tax credits and incentives when
+                                    you
+                                    upgrade your HVAC system. The Energy Efficient Home Improvement Credit offers up to
+                                    $600
+                                    for qualified air conditioners, gas furnaces, and boilers, and up to $2000 for
+                                    qualified
+                                    heat pumps. Don't miss out on these savings – let us help you maximize your
+                                    benefits!</p>
+
+
+
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br>
+
+
+
+                                <p><strong> When getting the best high-efficiency heat pump or heat pump repair in
+                                        Lenawee County,
+                                        the only name you need to know is Naugle Heating and Cooling.</strong></p>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
