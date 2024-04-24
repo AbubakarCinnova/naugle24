2d53ec3b5c3b3c712f3b3ed1a933cdd4e86631ef
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-24 16:00:22 +0500

    Create oil-furnaces.php

---
+++ oil-furnaces.php
@@ -0,0 +1,137 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling| Oil Furnace</title>
+
+<meta name="description"
+    content=" Naugle Heating and Cooling: Affordable Excellence for Your Oil Furnace Maintenance and Repair Needs in Lenawee County!">
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <!-- <img src="<?php echo $base_url ?>/img/lennox furnace banner (2).jpg"
+                                    alt="furnace banner" class="full-width-img">
+
+                                <br><br> -->
+
+
+                                <h2>Ready to boost comfort and savings?</h2>
+
+                                <p> Upgrade from your oil furnace to LP (propane) with Naugle Heating and Cooling. LP
+                                    (propane) furnaces offer superior efficiency, cutting energy use and costs for
+                                    long-term
+                                    savings and efficient heating!
+                                </p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br>
+
+
+                                <h2>Annual Oil Furnace Maintenance: Keeping Your Family Safe and Warm</h2>
+
+                                <p>Ensure your oil furnace is ready for action by scheduling maintenance or repair
+                                    service
+                                    today with Naugle Heating and Cooling. Annual maintenance; is crucial for extending
+                                    lifespan, enhancing energy efficiency, reducing costs, improving reliability, and
+                                    enhancing safety. Schedule your oil furnace maintenance with us for peace of mind
+                                    all
+                                    winter.</p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+
+                                <h2>Naugle OilGuard ClimateGuard: Oil Furnace Tune-Up</h2>
+
+                                <p> Inspect Thermostat (clean, check batteries, inspect wires)</p>
+
+
+                                <p><strong>Oil Burner Service:</strong></p>
+                                <ul>
+                                    <li>Replace Nozzle</li>
+                                    <li>Replace oil filter</li>
+                                    <li>Clean End cone</li>
+                                    <li>Adjust spark electrodes</li>
+                                    <li>Check the oil pump pressure</li>
+                                    <li>Test primary control</li>
+                                    <li>Check Ignition transformer</li>
+                                    <li>Clean air vanes and adjust for proper flame</li>
+                                    <li>Check the Heat Exchanger and Fire Chamber for Cracks and soot</li>
+                                    <li>Clean and inspect the flue/chimney</li>
+                                </ul>
+
+                                <p><strong>Blower Compartment:</strong></p>
+                                <ul>
+                                    <li>Clean blower compartment</li>
+                                    <li>Check blower wheel</li>
+                                    <li>Check the Blower motor and capacitor</li>
+                                    <li>Check Voltage and amp Draws</li>
+                                    <li>Check the blower belt (if equipped)</li>
+                                    <li>Oil motor and bearings if applicable</li>
+                                    <li>Test high-limit safety</li>
+                                    <li>Replace air filter</li>
+                                </ul>
+
+                                <p><strong>Oil Boiler:</strong></p>
+                                <ul>
+                                    <li>Check Zone Valves</li>
+                                    <li>Check temp and pressure</li>
+                                    <li>Check the pressure tank and fill the system</li>
+                                    <li>Clean Heat exchanger</li>
+                                    <li>Check relief Valve</li>
+                                    <li>Check piping for leaks</li>
+                                    <li>Check Circulator pump</li>
+                                    <li>Check air vents</li>
+                                    <li>Test Low water cutoff</li>
+                                </ul>
+
+
+
+
+                                <p> If it's <strong>steam:</strong> check the sight glass and follow manufacturer
+                                    recommendations for
+                                    drain
+                                    down and fill.</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+
+
+                                <p><strong> Naugle Heating and Cooling: Affordable Excellence for Your Oil Furnace
+                                        Maintenance and
+                                        Repair Needs in Lenawee County!</strong></p>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
