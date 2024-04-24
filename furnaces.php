ed2112d3200f68a46bf6a0d4d609928038fa722b
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-24 15:44:21 +0500

    furnaces page updated

---
+++ furnaces.php
@@ -0,0 +1,53 @@
+<?php include_once('header.php') ?>
+
+<title>Naugle Heating and Cooling| Furnaces</title>
+
+<meta name="description"
+    content=" Naugle Heating and Cooling: Lenawee County's Trusted Professional Choice for Affordable Furnace Sales and Installation for Over 45 Years.">
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/lennox furnace banner (2).jpg"
+                                    alt="furnace banner" class="img-responsive">
+
+                                <br><br>
+
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br>
+
+                                <p><strong>Naugle Heating and Cooling: Lenawee County's Trusted Professional Choice for
+                                        Affordable Furnace Sales and Installation for Over 45 Years.</strong></p>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
