87193a407d1140d4493880c43a20010fa495e200
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-24 15:52:37 +0500

    Update furnaces.php

--- furnaces.php
+++ furnaces.php
@@ -25,17 +25,178 @@
                             <div class="col-sm-12">
 
                                 <img src="<?php echo $base_url ?>/img/lennox furnace banner (2).jpg"
-                                    alt="furnace banner" class="img-responsive">
+                                    alt="furnace banner" class="full-width-img">
 
                                 <br><br>
 
+                                <h2>Understanding Your Home Furnace Options: Making Informed Choices</h2>
+                                <p>As a homeowner, navigating the world of heating systems can be daunting. From
+                                    variable speed heating to AFUE ratings, the options seem endless. Let us simplify
+                                    the process for you by shedding light on these furnace systems and terms, empowering
+                                    you to make informed decisions about your home's heating needs.</p>
+
+                                <h2>Furnace Efficiency Rating - AFUE</h2>
+                                <p>Understanding the Annual Fuel Utilization Efficiency (AFUE) rating is key to
+                                    assessing furnace efficiency. This metric measures how effectively a furnace
+                                    converts fuel into heat over a year. For example, a furnace with a 99 AFUE rating
+                                    delivers 99 cents worth of heat for every dollar spent on fuel, with minimal loss.
+                                    Keep in mind that AFUE ratings do not include heat loss through ducts.</p>
+
+                                <h2>Heat Loss Through Ducts</h2>
+                                <p>While the AFUE rating provides insight into furnace efficiency, addressing heat loss
+                                    through airflow ducts is vital. Leaky or poorly insulated air ducts can lead to
+                                    significant energy waste, undermining furnace efficiency. Proper sealing and
+                                    insulation are essential to minimize heat loss and maximize furnace effectiveness.
+                                    Consider evaluating your heating system with our expert guidance to optimize
+                                    efficiency and reduce energy costs.</p>
+
+                                <h2>Variable Capacity Furnaces: Dynamic Efficiency and Comfort</h2>
+                                <p>Variable capacity furnaces adjust their output from 35% to 100% based on heating
+                                    demand, achieving up to 99% AFUE ratings. These highly efficient furnaces ensure
+                                    long-term comfort and savings, representing the most modern and efficient options
+                                    available.</p>
+
+                                <h2>Smart Heating with Two-Stage Furnaces</h2>
+                                <p>Two-stage furnaces adapt to changing weather, offering balanced heating with two
+                                    output levels. They conserve energy and reduce noise during mild weather by
+                                    operating at a lower speed. When temperatures drop, they ramp up to full capacity
+                                    for maximum warmth. While the initial cost may be higher, the long-term savings on
+                                    heating bills make two-stage furnaces a wise investment.</p>
+
+                                <h2>Adaptive Airflow: Variable Speed Furnaces</h2>
+                                <p>Harnessing an innovative blower motor, variable-speed furnaces adjust airflow
+                                    dynamically. This adaptive system responds to changing heating demands, providing
+                                    consistent temperatures, improved indoor air quality, and reduced cycling. Compared
+                                    to traditional fixed-speed furnaces, they offer superior energy efficiency and
+                                    cost-effectiveness.</p>
+
+                                <h2>Understanding Single-Stage Gas Furnaces: Efficiency Comparison</h2>
+                                <p>Single-stage gas furnaces offer a budget-friendly option for simplicity and
+                                    affordability. However, unlike more energy-efficient systems like two-stage or
+                                    variable-speed gas furnaces, they lack energy-saving features. Operating at a
+                                    constant heat output, single-stage gas furnaces may result in higher monthly gas
+                                    bills compared to the more efficient systems.</p>
+
+                                <h2>Affordable Customized Heating Solutions</h2>
+                                <p>At Naugle Heating and Cooling, we specialize in personalized heating solutions
+                                    tailored to your home's unique characteristics. By considering factors like size,
+                                    design, climate, insulation, and occupants' behavior, we identify the most
+                                    cost-effective and energy-efficient option for you.</p>
+
+
+                                <p><strong>The amended Energy Efficient Home Improvement Credit, which began in 2023,
+                                        makes
+                                        certain products tax credit eligible. Qualified air conditioners, gas furnaces,
+                                        or
+                                        boilers may receive tax credits of up to $600, while qualified heat pumps could
+                                        be
+                                        eligible for tax credits of up to $2000.</strong></p>
+
+
+                                <h1>Our Most Popular Lennox Furnaces</h1>
+
+                                <h2>Lennox SLP99V Variable-Capacity Gas Furnace</h2>
+                                <ul>
+                                    <li>Up to 99% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Stages of Heating: Modulating</li>
+                                    <li>SilentComfort Technology and Fully Insulated Cabinet</li>
+                                    <li>Blower Motor: Variable Speed</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>Ultra-Low Continuous Fan</li>
+                                    <li>Ignition SureLight Silicon Nitride Ignitor</li>
+                                    <li>Dave Lennox Signature® Collection models come with a 10-Year Limited Warranty on
+                                        covered components. Furnace heat exchangers offer either a 20-year or A Lifetime
+                                        Limited Warranty</li>
+                                    <li>Tax Credit Eligible</li>
+                                </ul>
+
+                                <h2>Lennox EL297V High-Efficiency, Two-Stage Gas Furnace</h2>
+                                <ul>
+                                    <li>Up to 97% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Stages of Heating: Two Stages</li>
+                                    <li>SilentComfort Technology and Sound Absorbing Insulation</li>
+                                    <li>Blower Motor: Variable Speed</li>
+                                    <li>Enhanced Humidity Control</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>Ultra-Low Continuous Fan</li>
+                                    <li>Ignition SureLight Silicon Nitride Ignitor</li>
+                                    <li>A 10-year Extended Limited Warranty is available with product registration on
+                                        covered components.</li>
+                                    <li>Heat Exchanger Limited Lifetime Warranty</li>
+                                    <li>Tax Credit Eligible</li>
+                                </ul>
+
+                                <h2>Lennox ML296V Variable Speed Two-Stage Gas Furnace</h2>
+                                <ul>
+                                    <li>Up to 96% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Stages of Heating: Two Stages</li>
+                                    <li>Steel Cabinet</li>
+                                    <li>Blower Motor: Variable Speed</li>
+                                    <li>Enhanced Humidity Control</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>Ultra-Low Continuous Fan</li>
+                                    <li>Dual Fuel Capable</li>
+                                    <li>Ignition SureLight Silicon Nitride Ignitor</li>
+                                    <li>A 10-Year Extended Limited Warranty is available with product registration.</li>
+                                    <li>Heat Exchanger: 20-Year Limited Warranty</li>
+                                    <li>Tax Credit Eligible</li>
+                                </ul>
+                                <br>
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br>
+
+
+
+                                <h1>Our Most Popular Carrier Furnaces</h1>
+
+                                <h2>Carrier Infinity® 98 Gas Furnace 59 MN7</h2>
+                                <ul>
+                                    <li>Up to 98.5% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Fully Insulated Cabinet</li>
+                                    <li>Stages of Heating: Modulating</li>
+                                    <li>Variable Speed Motor</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>10-year parts limited warranty upon timely registration and lifetime heat
+                                        exchanger limited warranty upon timely registration.</li>
+                                    <li>Tax Credit Eligible</li>
+                                </ul>
+
+                                <h2>Carrier Infinity® 96 Gas Furnace 59TN6</h2>
+                                <ul>
+                                    <li>Up to 96.7% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Fully Insulated Cabinet</li>
+                                    <li>Stages of Heating: Two Stage</li>
+                                    <li>Variable Speed Motor</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>10-year parts limited warranty upon timely registration and lifetime heat
+                                        exchanger limited warranty upon timely registration.</li>
+                                </ul>
+
+                                <h2>Carrier Comfort™ 96 Gas Furnace 59SC6</h2>
+                                <ul>
+                                    <li>Up to 97% AFUE</li>
+                                    <li>Energy Star Rated</li>
+                                    <li>Fully Insulated Cabinet</li>
+                                    <li>Stages of Heating: Single Stage</li>
+                                    <li>Multi 18-speed Motor</li>
+                                    <li>Air Filter and Humidifier Compatible</li>
+                                    <li>10-year parts limited warranty upon timely registration and lifetime heat
+                                        exchanger limited warranty upon timely registration.</li>
+                                </ul>
 
 
 
                                 <br>
                                 <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
                                     Quote</a>
-                                <br>
+                                <br><br>
 
                                 <p><strong>Naugle Heating and Cooling: Lenawee County's Trusted Professional Choice for
                                         Affordable Furnace Sales and Installation for Over 45 Years.</strong></p>
