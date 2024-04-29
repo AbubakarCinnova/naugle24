a43319c68f366bb5babc81009857377ee5ba1ce3
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-29 09:58:21 +0500

    smart page updated

---
+++ smart-thermostats.php
@@ -0,0 +1,55 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling| Smart Thermostats</title>
+
+<meta name="description"
+    content=" Naugle Heating and Cooling: Lenawee County's Trusted Professional Choice for Affordable Smart Thermostat Sales and Installations.">
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/smart thermostat naugle.jpg"
+                                    alt="page banner smart thermostats" class="full-width-img">
+
+                                <br><br>
+
+
+
+
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
