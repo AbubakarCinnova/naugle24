9ff9fc477c52a3eb78e704814b0bb550634cb1a9
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-03 12:26:18 +0500

    special page updated

---
+++ specials.php
@@ -0,0 +1,33 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling | Specials </title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <br>
+                                <img src="<?php echo $base_url ?>/img/Lennox Dealer Flyers naugle (1)_page-0001.jpg"
+                                    alt="page banner special" class="full-width-img">
+
+                                <br>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
