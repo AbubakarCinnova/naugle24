98d61d8106aac218f618ca5265092ccb8235872f
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-03 15:32:27 +0500

    saline updated

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
                         <li><a href="<?php echo $base_url ?>/service-area/manchester.php">Manchester, MI</a></li>
                         <li><a href="<?php echo $base_url ?>/service-area/adrian.php">Adrian , MI</a></li>
                         <li><a href="<?php echo $base_url ?>/service-area/dundee.php">Dundee , MI</a></li>
+                        <li><a href="<?php echo $base_url ?>/service-area/saline.php">Saline , MI</a></li>
 
                     </div>
                     <!--        <div class="item-list-inner">
