6474741d282813410e85c7fc56ec98bbe351fd41
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-06 16:40:23 +0500

    Update repair.php

---
+++ repair.php
@@ -0,0 +1,118 @@
+<?php include_once('header.php') ?>
+
+<title>Naugle Heating and Cooling | Repair</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+                                <h2>Exemplary Repair Services for Your HVAC Systems</h2>
+
+                                <p>Step into a world of prompt and reliable repair services for your residential and
+                                    commercial heating, cooling, and water heating systems with Naugle Heating and
+                                    Cooling. Our round-the-clock emergency repair services guarantee comfort precisely
+                                    when you need it most.</p>
+
+                                <h2>Furnace Repair Services</h2>
+
+                                <p>Place your trust in our seasoned technicians to meticulously diagnose and restore
+                                    your furnace or HVAC system. Whether it's electric, gas, or oil furnaces, heat
+                                    pumps, or boilers, we excel in reinstating peak efficiency and comfort in your home
+                                    or workplace.</p>
+
+                                <h2>Air Conditioner Repair Services</h2>
+
+                                <p>Are you feeling the heat from a malfunctioning air conditioning system? Fear not! Our
+                                    affable and adept technicians specialize in pinpointing and remedying all AC system
+                                    maladies, ensuring swift restoration to get you back chilling out.</p>
+
+                                <h2>Boiler Repair</h2>
+
+                                <p>Even the sturdiest boilers need a touch of TLC now and then. Rely on our expertise to
+                                    promptly tackle boiler hiccups, ensuring uninterrupted operation throughout the
+                                    year.</p>
+
+                                <h2>Water Heater Emergencies</h2>
+
+                                <p>Count on us to provide swift and effective solutions. Our skilled technicians are
+                                    equipped to handle water heater emergencies, ensuring your hot water heater needs
+                                    are met promptly and efficiently.</p>
+
+                                <h2>Annual Climateguard Maintenance Program</h2>
+
+                                <p>Embrace the promise of enduring performance for your HVAC setup through our
+                                    all-encompassing maintenance program. Regular servicing not only slashes energy
+                                    costs and future repair bills but also extends the operational lifespan of your
+                                    cherished system.</p>
+
+                                <h2>Our Offerings Include:</h2>
+
+                                <ul>
+                                    <li>Air Conditioning Systems</li>
+                                    <li>Central Air Systems</li>
+                                    <li>Boilers</li>
+                                    <li>Water Heaters</li>
+                                    <li>Oil Furnaces</li>
+                                    <li>Commercial HVAC</li>
+                                    <li>AC Recharging</li>
+                                    <li>Ductless Mini Split Systems</li>
+                                    <li>Ductwork Repairs and Replacements</li>
+                                    <li>UV Germicidal Lights</li>
+                                    <li>Thermostats</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Smart Thermostats</li>
+                                    <li>Indoor Air Quality Systems</li>
+                                    <li>24/7 Emergency Services</li>
+                                </ul>
+
+                                <h2>Why Choose Us?</h2>
+
+                                <ul>
+                                    <li>Top-notch customer service</li>
+                                    <li>Over 45 years of HVAC repair experience</li>
+                                    <li>Free estimates on new HVAC system installations</li>
+                                    <li>Flexible financing options with approved credit</li>
+                                    <li>Local licensed Lennox and Carrier dealer</li>
+                                    <li>Year-round promotions and rebates</li>
+                                    <li>Competitive Pricing</li>
+                                    <li>Family-owned and operated</li>
+                                    <li>Consistently rated 5 stars by our customers</li>
+                                </ul>
+
+
+
+                                <br>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Service</a>
+                                <br>
+
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
