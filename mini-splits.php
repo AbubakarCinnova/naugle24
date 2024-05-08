f0dd5f0969015796d6242fd5558ec18fc653204e
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-08 18:17:04 +0500

    Create mini-splits.php

---
+++ mini-splits.php
@@ -0,0 +1,112 @@
+<?php include_once('header.php') ?>
+
+<title>Naugle Heating and Cooling | Ductless Mini Split</title>
+<meta name="description"
+    content="Ductless mini split heating and cooling systems are highly efficient heating and cooling systems that deliver warm or cool conditioned air directly into different rooms in your Michigan home, without the use of ducting " />
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+
+                                <h2>What is a Ductless Mini-Split System?</h2>
+                                <p>A ductless mini-split system is a type of heating and cooling system that does not
+                                    require the use of ducts. It uses
+                                    a heat pump to efficiently deliver warm or cool air directly into various rooms in a
+                                    home or building. Also known
+                                    as mini-split or multi-split heat pumps or ductless mini-split air conditioning
+                                    systems.</p>
+                                <br>
+                                <!-- Insert your mini split Lennox picture here -->
+                                <img src="https://www.888797jack.com/img/lennox-mini-split-tc1.jpg" alt="water heater"
+                                    style="width:70%" ;="">
+                                <br>
+                                <p>Ductless mini-split heating and cooling units use small heating and cooling air
+                                    handlers for every individual
+                                    room, so installing a ductless mini-split system makes it easy to set up heating and
+                                    cooling zones in your home.
+                                    This means each room in your home can be at a different temperature and comfort
+                                    level.</p>
+
+                                <p>Some of our mini-split ductless systems can accommodate up to four indoor units that
+                                    can provide heating or cooling,
+                                    and each of these air handlers can be controlled individually via its own wireless
+                                    thermostat.</p>
+
+                                <p>Since mini splits have no need for ducts, they avoid the energy losses associated
+                                    with the ductwork of central air
+                                    systems. Heated or cooled air lost through leaky or uninsulated ducts in your home
+                                    can account for more than 30%
+                                    of energy consumption for space conditioning, especially if the ducts are in an
+                                    unconditioned space such as an
+                                    attic. So this makes a ductless mini-split highly energy efficient.</p>
+
+                                <p>Popular uses of ductless mini-split systems include:</p>
+                                <ul>
+                                    <li><strong>Older Homes </strong>– Great heating and cooling solution for older
+                                        homes that were not
+                                        constructed with a ductwork
+                                        system in place. For example, an older home with a boiler-style heating system
+                                        has no ducting.</li>
+                                    <li><strong>Sun Rooms or Enclosed Patios </strong>– Ductless mini-splits can provide
+                                        efficient
+                                        year-round comfort with heating and
+                                        cooling.</li>
+                                    <li><strong>Outbuildings </strong>– Ductless mini and multi-splits are great for
+                                        guest houses and
+                                        workshops, providing both heating
+                                        and cooling.</li>
+                                    <li><strong>New Additions and Remodels </strong>– Ductless mini-splits are the
+                                        perfect heating and
+                                        cooling solution for remodels
+                                        or new additions to the home.</li>
+                                    <li><strong>Home Offices </strong>-&lt; A ductless mini-split heating and cooling
+                                        system will make your home office comfortable and help you be more
+                                        productive year-round.</li>
+                                    <li><strong>Man Caves </strong>- With so many heat-producing electronics, a ductless
+                                        mini-split is a
+                                        perfect solution to keep your
+                                        man cave comfortable all year long.</li>
+                                    <li><strong>Home Theater Rooms </strong>- Entertain in comfort.</li>
+                                    <li><strong>Garages </strong>- Work on your car in the middle of Winter or the
+                                        hottest day of the
+                                        Summer and be truly comfortable.</li>
+                                </ul>
+                                <br>
+                                <p>For more information on how you can save with the ductless mini-split heating and
+                                    cooling system, contact the ductless
+                                    mini-split heating and air conditioning experts at Naugle Heating and Cooling.</p>
+                                <br>
+                                <!-- Embed the YouTube video here -->
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/1amhrr52hvM?t=1s"
+                                    frameborder="0" allowfullscreen=""></iframe>
+
+                                <br>
+
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
