af25b91929859df81dacfc6b1c587a6cc6cf2ea6
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-16 10:26:11 +0500

    updated cooling page

---
+++ cooling.php
@@ -0,0 +1,110 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling| Cooling</title>
+
+
+<meta name="description"
+    content=" At Naugle Heating and Cooling, our friendly and skilled technicians are ready to diagnose and swiftly resolve any issues with your air conditioning system." />
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+
+                                <h2>Air Conditioner Repair Services</h2>
+
+                                <img src="<?php echo $base_url ?>/img/Need_a_new_ac-1 (2) (1).jpg" alt="ac banner"
+                                    class="short-img">
+
+                                <p>Are you struggling with an air conditioning system that won't cool? There's no need
+                                    to sweat it out! At Naugle Heating and Cooling, our friendly and skilled master
+                                    technicians are ready to diagnose and swiftly resolve any issues with your air
+                                    conditioning system, ensuring you're back to cool comfort in no time!</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Service</a>
+                                <br>
+
+
+                                <h2>Air Conditioner Maintenance</h2>
+                                <p>Don't let a breakdown ruin your summer vibe! With Naugle Heating and Cooling's expert
+                                    maintenance, you can keep your AC running smoothly all season long. Our ClimateGuard
+                                    maintenance plans are designed to save you money and hassle by preventing unexpected
+                                    repairs. Say goodbye to sweltering days and hello to worry-free cooling! <a
+                                        href="<?php echo $base_url ?>/service-plan.php">Learn
+                                        More about our ClimateGuard Maintenance plan</a></p>
+
+                                <h2>High-Efficiency Air Conditioning Systems that Save You Money</h2>
+                                <p>Are you tired of high energy bills and inefficient cooling? It's time to upgrade to a
+                                    high-efficiency air conditioning system from Naugle Heating and Cooling. Our Lennox
+                                    and Carrier high-efficiency central air systems are not just about cooling your
+                                    home; they're about saving you money while keeping you comfortable. <a
+                                        href="<?php echo $base_url ?>/air-conditioners.php">Learn more about our air
+                                        conditioning products</a></p>
+
+                                <h2>Ductless Mini Split</h2>
+                                <p>Say goodbye to hot spots and hello to personalized comfort with our ductless
+                                    mini-split systems! Perfect for homes without ductwork and for targeted cooling in
+                                    specific areas, these systems offer efficient, whisper-quiet comfort all year round.
+                                    <a href="<?php echo $base_url ?>/mini-splits.php">Learn more
+                                        about Ductless mini splits</a>
+                                </p>
+
+                                <h2>We Offer Sales, Service, and Installation of:</h2>
+                                <ul>
+                                    <li>Air Conditioning Installation</li>
+                                    <li>Residential and Commercial HVAC Replacement</li>
+                                    <li>AC Recharging</li>
+                                    <li>Air Conditioning Repairs</li>
+                                    <li>High-Efficiency Air Conditioning</li>
+                                    <li>Maintenance Plans</li>
+                                    <li>Ductless Mini Split Systems</li>
+                                    <li>Central Air Repair</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Ductwork Repairs and Replacements</li>
+                                    <li>UV Germicidal Lights</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Affordable Pricing</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>Carrier Dealer</li>
+                                    <li>Indoor Air Quality Installation</li>
+                                    <li>Emergency Service</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Whole House Air Purifiers</li>
+                                </ul>
+
+
+
+                                <br>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/specials.php" class="btn btn-warning">See Our
+                                    Specials</a>
+                                <br>
+
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
