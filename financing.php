d6517eae10c2fd8064fd5d81c792f2b6284d7cc3
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-16 13:01:16 +0500

    updated financing

---
+++ financing.php
@@ -0,0 +1,51 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling | Financing </title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+                                <img src="<?php echo $base_url ?>/img/financing-banner-1.jpg" alt="Financing"
+                                    class="full-width-img">
+                                <br>
+
+                                <p>A new home heating and cooling system is one of the most important investments you'll
+                                    make, one that can deliver energy and cost savings for many years to come. You don't
+                                    have to let a small budget keep you from getting the most efficient solution for
+                                    your household. That's why we offer flexible financing options.</p>
+
+
+                                <p><strong>Call us today to find out the financing options we have available for you
+                                    </strong></p>
+                                <p>All financing options are subject to credit approval.</p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact
+                                    Us</a>
+                                <br>
+
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
