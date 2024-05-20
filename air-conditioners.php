6a968896904a73fb70bccfcc616e7c26c877645b
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-20 12:07:03 +0500

    updated air conditioner

---
+++ air-conditioners.php
@@ -0,0 +1,202 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling | Air Conditioner </title>
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+                                <h2>Is it time for a new air conditioner?</h2>
+                                <p>When summer temperatures and humidity arrive in Michigan, you need an air conditioner
+                                    ready to perform to your expectations. If your current central air system isn’t up
+                                    to the task, it’s time to upgrade. Naugle Heating and Cooling installs advanced,
+                                    affordable, energy-efficient, high-performance air conditioners and cooling systems
+                                    for homes and businesses.</p>
+
+                                <p>Our comfort designers will tailor your needs to the perfect-sized system for your
+                                    home or business. We are committed to giving our customers the right air
+                                    conditioning solution for them at fair prices.</p>
+
+                                <h2>So What Does the SEER Rating of an AC system Mean?</h2>
+                                <p>SEER stands for "Seasonal Energy Efficiency Ratio," and it is a measure of the
+                                    cooling output of an air conditioning system over a typical cooling season, divided
+                                    by the amount of energy consumed in watt-hours.</p>
+
+                                <h2>Variable-Capacity AC Compressor</h2>
+                                <p>The Variable-capacity compressor, also called Variable Speed, is currently our most
+                                    efficient and quietest option. It can operate at various speeds depending on the
+                                    cooling requirements, ranging from 35-100%. Compared to two-stage systems, it offers
+                                    a wider range of speeds, enabling the air conditioning to maintain lower speeds for
+                                    longer durations while providing the option for higher speeds when the outdoor
+                                    temperature rises. As a result, variable-speed AC systems run continuously without
+                                    frequent on/off cycles and only at the required speed to keep you comfortable,
+                                    making them the most energy-efficient among the three types of AC systems.</p>
+
+                                <h2>Two-Stage / Multi-Stage AC Compressor</h2>
+                                <p>A two-stage AC compressor operates at two levels: high for extreme heat and low for
+                                    milder days. It provides efficient cooling by running at 70% initially, switching to
+                                    100% if needed. This setup results in longer operating times, maintaining a
+                                    consistent indoor temperature. Compared to single-stage compressors, it's more
+                                    efficient and reduces wear and tear by cycling less frequently. Additionally, it
+                                    removes more humidity due to extended operation at the lower level.</p>
+
+                                <h2>Single-Stage AC Compressor</h2>
+                                <p>A single-stage AC compressor operates at a fixed full-cold speed, controlled by the
+                                    thermostat turning the system on and off to regulate temperature. In Michigan, most
+                                    homes with central air conditioning use a single-stage compressor, which has been
+                                    the standard for several decades. The latest single-stage models, while very
+                                    affordable, feature cutting-edge technology, making them considerably more
+                                    energy-efficient than older single-stage compressors, which had much lower SEER
+                                    ratings. However, despite these improvements, they still fall short in efficiency
+                                    compared to two-stage or variable-capacity compressors.</p>
+
+                                <h2>Customized Heating Solutions</h2>
+                                <p>Naugle Heating and Cooling's master technicians understand the critical importance of
+                                    load calculation in HVAC system design. They meticulously analyze factors such as
+                                    space size, insulation, climate, windows, and occupancy to accurately determine the
+                                    heating or cooling capacity required for each home. By ensuring that the equipment
+                                    installed is neither oversized nor undersized, Naugle's technicians optimize energy
+                                    efficiency, comfort, and the longevity of the system. This meticulous approach,
+                                    combined with affordable and transparent pricing, guarantees that customers receive
+                                    tailored solutions that meet their needs while staying within their budget.</p>
+
+
+
+                                <h1>Our Most Popular Lennox Air Conditioner</h1>
+
+                                <p> <img src="<?php echo $base_url ?>/img/lennox-ac's.png" alt="lennox-ac's"
+                                        class="full-width-img"></p>
+
+                                <h2>Lennox Signature Model SL28XCV</h2>
+                                <p>The most precise and efficient air conditioner available</p>
+                                <ul>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>Up to 28 SEER</li>
+                                    <li>SEER2 Up to 25.8</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level: As low as 59 dBA</li>
+                                    <li>Cabinet: PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort-enabled technology</li>
+                                    <li>10-Year Limited Warranty on compressor and covered components</li>
+                                </ul>
+
+                                <h2>Lennox Elite Model EL23XCV</h2>
+                                <p>High-efficiency, digitally-enabled, variable-capacity air conditioner</p>
+                                <ul>
+                                    <li>22.00 SEER2</li>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level: As low as 59 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort-enabled technology</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
+                                        available with product registration.</li>
+                                </ul>
+
+                                <h2>Lennox Merit Model EL14XC1</h2>
+                                <p>Efficient, durable air conditioner</p>
+                                <ul>
+                                    <li>17 SEER</li>
+                                    <li>Efficiency Rating: SEER 2 13.4</li>
+                                    <li>Stages of Cooling: Single Stage</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level: As low as 73 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>All Merit models come with a 5-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
+                                        available with product registration.</li>
+                                </ul>
+
+                                <p><strong>Contact Us for a Free in-Home Estimate For a New Home Comfort System</strong>
+                                </p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" target="_self"
+                                    class="btn btn-warning">Request Quote
+                                </a>
+                                <br>
+
+
+
+
+
+                                <h1>Our Most Popular Carrier Air Conditioners</h1>
+                                <p><img src="<?php echo $base_url ?>/img/carrier ac's.jpg"
+                                        alt="Carrier Air Conditioners"></p>
+
+                                <h2>Carrier Infinity® 26 Air Conditioner 24VNA6</h2>
+                                <p>with Greenspeed® Intelligence</p>
+                                <ul>
+                                    <li>Stages of Cooling: Variable-Speed</li>
+                                    <li>Up to 24 SEER2</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level: As low as 51 dBA</li>
+                                    <li>Silencer System II Design with Compressor Sound Blanket</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>10-year parts†</li>
+                                    <li>10-year unit replacement (24VNA6 compressor failure only)</li>
+                                </ul>
+
+                                <h2>Carrier Performance™ 16 Central Air Conditioner 24ACC4</h2>
+                                <ul>
+                                    <li>Stages of Cooling: Single Stage</li>
+                                    <li>Up to 17 SEER</li>
+                                    <li>Sound Level: As low as 72 dBA</li>
+                                    <li>Compressor Sound Blanket</li>
+                                    <li>10-year parts†</li>
+                                </ul>
+
+                                <p>Contact Us for a Free in-Home Estimate For a New Home Comfort System</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" target="_self"
+                                    class="btn btn-warning">Request Quote
+                                </a>
+                                <br>
+
+                                <small>† Upon timely registration. The warranty period is five years if not registered
+                                    within 90 days of installation except in jurisdictions where warranty benefits
+                                    cannot be conditioned upon registration. See the limited warranty certificate for
+                                    complete details and restrictions.</small>
+                                <br>
+
+                                <p><a href="<?php echo $base_url ?>/specials.php" class="btn btn-warning">See
+                                        Our Specials</a></p>
+
+                                <br>
+
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
