eb20cb4aee767ba41a622d9494bc35fba628dd86
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-20 16:33:13 +0500

    grass updated

--- footer.php
+++ footer.php
@@ -39,6 +39,7 @@
                         <li><a href="<?php echo $base_url ?>/service-area/hudson.php">Hudson, MI</a></li>
                         <li><a href="<?php echo $base_url ?>/service-area/blissfield.php">Blissfield, MI</a></li>
                         <li><a href="<?php echo $base_url ?>/service-area/onsted.php">Onsted, MI</a></li>
+                        <li><a href="<?php echo $base_url ?>/service-area/grass-lake.php">Grass Lake, MI</a></li>
 
                     </div>
 
