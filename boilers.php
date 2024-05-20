a4774708e9472aafdf473863785a1e7b8f21d039
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-05-20 12:21:43 +0500

    Create boilers.php

---
+++ boilers.php
@@ -0,0 +1,95 @@
+<?php include_once('header.php') ?>
+<title>Naugle Heating and Cooling| Boilers</title>
+
+<meta name="description"
+    content=" From high-efficiency boiler installations to prompt repair and expert sales guidance, trust Naugle to keep your home warm and efficient. Contact us today for top-quality boiler solutions tailored to your needs and budget.">
+
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+
+
+                                <h2>Boiler Sales and Installation</h2>
+
+
+                                <p>Revitalize your home comfort with Naugle Heating and Cooling's comprehensive boiler
+                                    services. Whether you need a high-efficiency boiler installation, reliable repair,
+                                    or
+                                    expert sales guidance, we have you covered.</p>
+
+
+
+                                <p>Upgrade to one of our high-efficiency boilers to enjoy significant energy savings and
+                                    consistent warmth throughout your home. Our skilled technicians will handle every
+                                    aspect
+                                    of the installation process with precision and care, ensuring optimal performance
+                                    and
+                                    efficiency.</p>
+
+
+
+
+                                <h2>Boiler Maintenance and Repair</h2>
+
+                                <p>Trust our experienced team to provide prompt and thorough repair services whenever
+                                    you
+                                    encounter issues. We understand the importance of a smoothly running boiler, and
+                                    we'll
+                                    work diligently to restore its functionality as quickly as possible.</p>
+
+
+
+                                <h2>Looking to invest in a new boiler?</h2>
+
+                                <p>Our knowledgeable sales team will guide you through the selection process, helping
+                                    you
+                                    find the perfect solution to meet your needs and budget.</p>
+
+                                <p>Experience the difference with Naugle Heating and Cooling's top-quality boiler
+                                    services.
+                                    Contact us today to schedule your installation, repair, or consultation.</p>
+
+
+
+
+
+
+                                <p><strong>Contact Us for a Free in-Home Estimate For a New Home Boiler System</strong>
+                                </p>
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
